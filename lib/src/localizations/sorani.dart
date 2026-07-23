b1969f7ed03747075580674536062c90b12ec690
rezaiemehrnegar-rgb <rezaiemehrnegar@gmail.com>
2026-07-23 20:01:52 +0330

    7/23/2026

--- lib/src/localizations/sorani.dart
+++ lib/src/localizations/sorani.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shamsi_date/shamsi_date.dart';
-
 import '../material/date.dart';
 
 class SoraniMaterialLocalizations extends DefaultMaterialLocalizations {
